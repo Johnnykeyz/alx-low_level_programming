f92a6a4123ed956fe3a0a773ceeec42e1f69cd38
Johnnykeyz <adebulejohnfav@gmail.com>
2022-09-08 15:01:36 -0700

    Printing using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #include <stdio.h>
 /**
 *main print the string in the puts function
@@ -6,7 +8,6 @@
 */
 
 int main(void) 
-{
-    puts("\"Programming is like building a multilingual puzzle");
+{ puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
