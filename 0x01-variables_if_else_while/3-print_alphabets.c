c8edfc3635d122b33daea19b9630e3cd574ef688
Johnnykeyz <adebulejohnfav@gmail.com>
2022-09-09 16:56:19 -0700

    Capital letter alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets capital letter
+ *
+ * Description: using main function
+ * this program prints all the alphabet in CAPITAL LETTER
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
