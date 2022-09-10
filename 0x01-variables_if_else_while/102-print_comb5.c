27ae7fb28511f6e7f98085a6f00df22c907744d1
Johnnykeyz <adebulejohnfav@gmail.com>
2022-09-09 20:15:13 -0700

    Print with space

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,10 +16,10 @@ for (j = 0; j < 100; j++)
 {
 if (i < j)
 {
-	putchar((i/10) + 48);
+	putchar((i / 10) + 48);
 	putchar((i % 10) + 48);
 	putchar(' ');
-	putchar((i/10) + 48);
+	putchar((i / 10) + 48);
 	putchar((i % 10) + 48);
 if (i != 98 || j != 99)
 {
