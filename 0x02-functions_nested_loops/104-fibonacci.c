23aa288ff18facb8792b2db7970106b36d803985
Johnnykeyz <adebulejohnfav@gmail.com>
2022-09-15 03:18:59 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,15 +8,14 @@
 int main(void)
 {
 	int inc;
-	unsigned long n1 = 0, n2 = 1, n3;
-	
+	unsigned long long n1 = 0, n2 = 1, n3;
+
 for (inc = 0; inc < 98; inc++)
 {
 	n3 = n1 + n2;
 	printf("%lu", n3);
 	n1 = n2;
 	n2 = n3;
-	
 	if (inc == 97)
 		printf("\n");
 	else
