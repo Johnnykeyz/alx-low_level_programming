ddf155bbe36af936e3c2ca5ab4f479b4b18ff008
Johnnykeyz <adebulejohnfav@gmail.com>
2022-09-28 17:14:10 -0700

    Last set of Mandatory class on recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * helper - does it for me
+ * @i: helper int
+ * @n: integer in testing
+ * Return: value
+ */
+int helper(int i, int n)
+{
+	if (n % i == 0 && n != i)
+		return (0);
+	if (n % i != 0 && i < n)
+		return (helper(i + 1, n));
+	return (1);
+}
+/**
+ * is_prime_number - checks if it is a prime number
+ * @n: integer to compare
+ * Return: boolean
+ */
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (n < 2)
+		return (0);
+	return (helper(i, n));
+}
