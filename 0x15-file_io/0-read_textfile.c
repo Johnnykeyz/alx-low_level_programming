0cde499ff83c1f674002f3bc0cecc72a59c9be4b
Johnnykeyz <adebulejohnfav@gmail.com>
2022-10-31 14:59:38 -0700

    my correct file io project

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,36 @@
-#include "main.h""
+#include "main.h"
+
 /**
- * read_textfile - read a file.
- * @filename: the file to read.
- * @letters: the number of chars to read
- * Description: read a file
- * section header: the header of this function is holberton.h
- * Return: this return the number of chars.
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, _read, _write;
-	char *buffer;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
+	fd = open(filename, O_RDONLY);
 
-	fd = open(filename, O_RDONLY, 600);
 	if (fd == -1)
-	{
-		free(buffer);
 		return (0);
-	}
-	_read = read(fd, buffer, letters);
-	if (_read == -1)
-	{
-		free(buffer);
-		return (0);
-	}
 
-	_write = write(STDOUT_FILENO, buffer, _read);
-	if (_write == -1 || _write != _read)
-	{
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
 
-	free(buffer);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
-	return (_write);
+
+	free(buf);
+
+	return (nwr);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,38 @@
 #include "main.h"
-#include "holberton.h"
 
 /**
- *	create_file - create a file and write in it
- * @filename:file to be created
- * @text_content: content of the file created
- * Return: 1 or -1;
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len, w;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
-	{
 		return (-1);
-	}
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len] != '\0'; len++)
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
-		w = write(fd, text_content, len);
-	}
-	if (w == -1)
-	{
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
-	}
+
 	close(fd);
+
 	return (1);
 }
